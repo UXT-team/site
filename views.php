eb7896a3212e17f93c03bbf6b82bbf598bcdf627
UXT-team <rufusnga@yahoo.com>
2016-08-05 16:11:17 +0300

    views

---
+++ views.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>views</title>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+     <!-- Bootstrap -->
+     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    
+</head>
+<body>
+<div class="well">
+<div clas="col-md-12">
+<div class="table-scrol">
+  <div class="table-responsive">
+    <table class="table table-bordered table-hover table-striped" style="table-layout:fixed">
+      <thead>
+        <tr class="info">
+         <th>Phone number</th>
+            <th>processed</th>
+            <th>In date</th>
+            <th>Text Message</th>
+            <th>Service_id</th>
+            <th>Processed1</th>
+            <th>sender_name</th>
+            <th>Link_id</th></tr>
+            <th>product_id</th></tr>
+            <th>Client</th></tr>
+            <th>Service_type</th></tr>
+            <th>Timestamp</th></tr>
+            <th>sent_flag</th></tr>
+            <th>user_id</th></tr>
+            <th>Coproc</th></tr>
+            <th>Correlator</th></tr>
+            <th>Sync</th></tr>
+            <th>Id</th></tr>
+            
+
+      </thead>
+      <?php
+      //connect to db
+      //connect to db
+      include 'database/db_connection.php';
+
+      //select query to view users
+      $view_admin_query = " select * from incoming_sms";
+      //run the sql query
+
+      $run = mysqli_query($con, $view_admin_query);
+
+      //while fetches  the result and store in an array row
+
+      while ($row = mysqli_fetch_array($run)) {
+        
+        $phonenumber = $row[0];
+        $processed = $row[1];
+        $indate = $row[2];
+        $textmesage = $row[3];
+        $Service_id= $row[4];
+        $Processed1= $row[5];
+        $sender_name = $row[6];
+        $Link_id = $row[7];
+        $product_id = $row[8];
+        $Client = $row[9];
+        $Service_type = $row[10];
+        $send_time= $row[11];
+        $Timestamp = $row[12];
+        $sent_flag = $row[13];
+        $user_id = $row[14];
+        $Coproc = $row[15];
+        $Correlator = $row[16];
+        $Sync= $row[17];
+        $Id = $row[18];
+       
+      
+
+       ?>
+      
+       <tr>
+         <!-- show those results in the table -->
+         <td><?php echo $phonenumber; ?></td>
+         <td><?php echo $processed; ?></td>
+         <td><?php echo $indate; ?></td>
+         <td><?php echo $textmesage; ?></td>
+         <td><?php echo $Service_id; ?></td>
+         <td><?php echo $Processed1; ?></td>
+         <td><?php echo $sender_name; ?></td>
+         <td><?php echo $Link_id; ?></td>
+         <td><?php echo $product_id; ?></td>
+         <td><?php echo $Client; ?></td>
+         <td><?php echo $Service_type; ?></td>
+         <td><?php echo $send_time; ?></td>
+         <td><?php echo $Timestamp; ?></td>
+         <td><?php echo $sent_flag; ?></td>
+         <td><?php echo $user_id; ?></td>
+         <td><?php echo $Coproc; ?></td>
+         <td><?php echo $Correlator; ?></td>
+         <td><?php echo $Sync; ?></td>
+         <td><?php echo $Id; ?></td>
+        <!--<td><a href="deleteadmin.php?del=<#?php echo $admin_id ?>"><button class="btn btn-warning">Delete Admin</button></a></td>-->
+       </tr>
+       <?php } ?>
+    </table>
+  </div>
+</div>
+</div><!--col-md-12 -->
+</div><!--well -->
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</body>
+</html>
