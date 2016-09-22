c4fbfa69d1d654df685add77430c54995916c296
ruddy12 <rufusngash@gmail.com>
2016-09-22 13:50:51 +0300

    mkonnect folder

---
+++ mkonnect/ict.php
@@ -0,0 +1,126 @@
+
+<div id="ict" class="collapse">
+                <div class="card">
+                  <div class="card-content">
+                        <center>
+                    <p>It is a long established fact that a reader will be distracted by the readable content of </p>
+                        </center>
+
+                        <!--modal-->
+                        <div class="container">
+  <center><h2>Apply the Jobs by clicking the button below<span class="glyphicon glyphicon-hand-down"></span></h2>
+  <!-- Trigger the modal with a button -->
+
+  <button type="button" class="btn btn-warning btn-sm" data-toggle="modal" data-target="#IT" style="font-size:1em;">
+                        Apply
+                        </button>
+  </center>
+
+
+  <!-- Modal -->
+  <div class="modal fade" id="IT" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title"><span class="glyphicon glyphicon-pencil"></span><center>JOB APPLICATION</center></h4>
+        </div>
+        <div class="modal-body">
+          
+           <div class="row">
+            <div class="col-md-12">
+                    <div class="col-md-6 col-md-offset-3 well">
+                       <form role="form" class="form-horizontal" action="register.php" method="POST" name="contactform">
+            <fieldset>
+    
+                <legend><center> <span class="glyphicon glyphicon-user"></span></center> <center> APPLY THE JOBS ABOVE</center> <center><span class="glyphicon glyphicon-user"></span></center> </legend>
+
+                <div class="form-group">
+                    
+                    <div class="col-md-12">
+                        
+                        <input class="form-control" name="txt_name" placeholder="Your Full Name" type="text" />
+                                           </div><!--col-md-12-->
+                </div><!--form-group-->
+
+                <div class="form-group">
+                    
+                    <div class="col-md-12">
+                        
+                        <input class="form-control" name="email" placeholder="Your Email" type="email" autofocus />
+                        
+                    </div><!--form-group-->
+                </div><!--form-group-->
+
+                <div class="form-group">
+                    
+                    <div class="col-md-12">
+                        
+                        <input class="form-control" name="txt_number" placeholder="Your Phone Number eg. +2547..." type="text" />
+                        
+                    </div><!--col-md-12-->
+                </div><!--form-group-->
+              
+                <div class="form-group">
+                    
+                    <div class="col-md-12">
+                        
+                        <input class="form-control" name="txt_pass" placeholder="Your Password" type="password" />
+                        
+                    </div><!--col-md-12-->
+                </div><!--form-group-->
+                 <div class="form-group">
+                    
+                    <div class="col-md-12">
+                        
+                        <input class="form-control" name="confirm_pass" placeholder="confirm Password" type="password" />
+                    </div><!--col-md-12-->
+                </div><!--form-group-->
+
+                <div class="form-group">
+                  
+                  <div class="col-md-12">
+                    
+                     <select class="form-control" name="txt_jobs" value="">
+                    
+                              <option>Accountant</option>
+                              <option>Clerk</option>
+                              <option>Cashier</option>
+                              <option>Banker</option>
+
+                            </select>
+
+                  </div><!--col-md-12-->
+
+                </div><!--form-group-->
+                
+                <div class="form-group">
+                    <div class="col-md-12">
+                       
+                         <input class="btn btn-lg btn-warning btn-block" type="submit" value="Register" name="register" style="font-size:1.25em;" >
+
+                    </div><!--col-md--12-->
+                </div><!--form-group-->
+            </fieldset>
+            </form>
+        </div><!--col-md--12-->
+        </div><!--col-md-12-->
+        </div><!--row-->
+
+
+        </div><!--modal-body-->
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div><!--modal-footer-->
+      </div><!--modal-content-->
+      
+    </div><!--modal-dialog-->
+  </div><!--modal-fade-->
+  
+</div><!--container-->
+
+                        </div><!--card-content-->
+                        </div><!--card-->
+                        </div><!--ICT-->
---
+++ mkonnect/index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Job Application</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" >
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet"  href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+     <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    
+</head>
+<body style=" background-color: #f7f7f7;">
+<section>
+
+<div class="container">
+
+  <div class="row">
+
+    <div class="col-md-12">
+
+      <div class="list-group">
+
+        <ul class="list-group">
+          <li class="list-group-item"><a data-toggle="collapse" href="#accounting" aria-expanded="false" aria-controls="collapseExample">
+
+          <center>Accounting</center></a>
+
+          </li><!--list-group-item-->
+          <?php include('accounts.php'); ?>
+
+
+                        <li class="list-group-item"><a data-toggle="collapse" href="#engineering" aria-expanded="false" aria-controls="collapseExample">
+
+                         <center>Engineering</center></a>
+
+                        <?php include('engineering.php'); ?>
+
+                       
+
+                       
+          
+
+                        
+                        <li class="list-group-item"><a data-toggle="collapse" href="#ict" aria-expanded="false" aria-controls="collapseExample">
+
+                      <center>ICT</center></a>
+
+                       </li><!--list-group-item-->
+                   <?php include('ict.php'); ?>
+
+                   <li class="list-group-item"><a data-toggle="collapse" href="#medicine" aria-expanded="false" aria-controls="collapseExample">
+
+                         <center>Medicine</center></a>
+
+                        <?php include('medicine.php'); ?>
+
+
+                       
+                   
+
+                        </div><!--list-group-->
+                        </div><!--col-md--12-->
+                        </div><!--row-->
+                        </div><!--container-->
+                        </section>
+
+                        
+        
+
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+ <!--<script src="http://getbootstrap.com/dist/js/bootstrap.js"></script>-->
+<script src="http://1000hz.github.io/bootstrap-validator/dist/validator.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+
+</body>
+</html>
