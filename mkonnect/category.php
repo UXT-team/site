169f1c0c46e0f79d46ab50db7d2c362e7cb9a67f
UXT-TEAM <rkahuko@uxt.co.ke>
2016-11-28 16:36:33 +0300

    category

--- mkonnect/category.php
+++ mkonnect/category.php
@@ -3,7 +3,7 @@
 
 if( $_POST ){
         session_start();//session starts here
-	
+
 	$category = $_POST['txt_category'];
 
         $_SESSION['txt_category']=$category;
@@ -13,7 +13,7 @@ if( $_POST ){
 	?>
        <form  action="savecategory.php" method="POST">
     <table class="table table-striped" border="0">
-    
+
     <tr>
     <td colspan="2">
     	<div class="alert alert-info">
@@ -21,18 +21,27 @@ if( $_POST ){
     	</div>
     </td>
     </tr>
-    
+
     <tr>
- 
+
     <td>Job Category</td>
-    
-     
 
-     <td><?php echo $category ?></td>
 
-    
+
+     <td><?php $con = mysqli_connect("localhost", "root", "", "m_konnect");
+		 $db = "job_category";
+
+		 $select_db=mysqli_select_db($con,$db);
+
+		 $query = "select * from job_category WHERE id=$category";
+		 $query1 = mysqli_query($con,$query);
+
+		 $row = mysqli_fetch_assoc($query1);
+			 echo $row['category']; ?></td>
+
+
     </tr>
-   
+
     <tr>
         <td>Save Changes</td>
         <td><button class="btn btn-success" name="save">Save Changes</button></td>
@@ -41,6 +50,5 @@ if( $_POST ){
     </table>
     </form>
     <?php
-	
-}
 
+}
