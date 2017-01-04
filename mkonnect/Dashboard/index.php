2f767cfac52d5dd431851f9ddccd02289dface71
ruddy12 <rufusngash@gmail.com>
2017-01-04 16:13:45 +0300

    dashboard

--- mkonnect/Dashboard/index.php
+++ mkonnect/Dashboard/index.php
@@ -103,8 +103,20 @@ if (!$_SESSION['email']) {
                    <a href="views/hospitality.php" class="list-group-item"><i class="glyphicon glyphicon-cloud-upload"></i>Hospitality</a>
                    <a href="views/others.php" class="list-group-item"><i class="glyphicon glyphicon-cloud-upload"></i>Others</a>
                 </div>
-                    
-                    <li class="list-group-item"><a href="help" style="color:#000000;"> <i class="glyphicon glyphicon-question-sign"></i>&nbsp;Help</a></li>
+                    <li class="list-group-item"> <button type="button" class="btn btn-warning btn-lg" data-toggle="collapse" data-target="#traffic">Traffic  <span class="glyphicon glyphicon-flash"></span></button> </li>
+                  </li>
+                    <div id="traffic" class="collapse">
+                      <li class="list-group-item"><a href="views/traffic.html" style="color:#000000;" >Facebook</a>
+                      <li class="list-group-item"><a href="traffic.html" style="color:#000000;" >Google SEO</a>
+                    </div>
+                    <li class="list-group-item"> <button type="button" class="btn btn-warning btn-lg" data-toggle="collapse" data-target="#activity">Activity  <span class="glyphicon glyphicon-flash"></span></button> </li>
+                  </li>
+                    <div id="activity" class="collapse">
+                      <li class="list-group-item"><a href="views/active.php" style="color:#000000;" >Active Job Seeker</a>
+                      <li class="list-group-item"><a href="views/dormant.php" style="color:#000000;" >Dormant Job seeker</a>
+                    </div>
+                    <li class="list-group-item"><a href="help" style="color:#000000;">
+                     <i class="glyphicon glyphicon-question-sign"></i>&nbsp;Help</a></li>
 
 
 
